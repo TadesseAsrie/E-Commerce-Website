e566de6cc62a6656547a50b8175c202ad32931c6
TadesseAsrie <tademr052@gmail.com>
2026-06-10 15:03:59 -0700

    routs

---
+++ ecommerce-frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import ProductDetails from "../pages/ProductDetails";
+import Cart from "../pages/Cart";
+import Wishlist from "../pages/Wishlist";
+import Checkout from "../pages/Checkout";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import NotFound from "../pages/NotFound";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/wishlist" element={<Wishlist />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
